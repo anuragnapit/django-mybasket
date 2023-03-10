0be6df41c9bba110ac2d2800cb1ebd3f061ca7af
anurag <anuragnapit.dev@gmail.com>
2023-03-10 22:58:29 +0530

    added debug change

--- MyBasket/settings.py
+++ MyBasket/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gdb9k*b#d-m+n024!cxbt&pf#_23$vk&^#5y#)e+ezj@zm15d*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
